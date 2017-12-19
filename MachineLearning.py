ffcb0163ee64cded6f2f1900f063990d0e2af622
JeffMacaluso <macaluso.jeff@gmail.com>
2017-12-19 17:52:20 -0600

    Added standard imports and options

---
+++ MachineLearning.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Formatting for seaborn plots
+sns.set_context("notebook", font_scale=1.1)
+sns.set_style("ticks")
+
+# Displays all dataframe columns
+pd.set_option('display.max_columns', None)
+
+%matplotlib inline
