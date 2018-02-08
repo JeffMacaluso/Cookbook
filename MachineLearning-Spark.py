4e4d70b6cbe300c8a93548020f490ea82c773e24
Jeff Macaluso <JeffMacaluso@users.noreply.github.com>
2018-02-08 10:50:28 -0800

    Added additional functions and structure

--- MachineLearning-Spark.py
+++ MachineLearning-Spark.py
@@ -1,24 +1,40 @@
 import sys
+import time
 
+print(time.strftime('%Y/%m/%d %H:%M'))
+print('OS:', sys.platform)
 print('Python:', sys.version)
 
+#################################################################################################################
+##### Cross Validation
 # Train/test split
-# We'll hold out 80% for training and leave 20% for testing 
 seed = 46
-(trainingDF, testDF) = df.randomSplit([8.0, 2.0], seed)  # Normalizes if it doesn't sum up to 1
-
+(trainingDF, testDF) = df.randomSplit([8.0, 2.0], seed)  # 80/20 split
 
 # Evaluating regression
 # https://spark.apache.org/docs/2.2.0/ml-tuning.html
 from pyspark.ml.evaluation import RegressionEvaluator
 
-regEval = RegressionEvaluator().setLabelCol('label')
-predictDF = model.transform(testDF)
-testRMSE = regEval.evaluate(predictDF)
+regEval = RegressionEvaluator().setLabelCol('label')  # Instantiating & setting the label column
+predictDF = model.transform(testDF)  # Generating predictions
+testRMSE = regEval.evaluate(predictDF)  # Gathering the RMSE
 print('The model had a RMSE on the test set of {0}'.format(testRMSE))
 
 
-# Grid Search CV
+# K-folds
+from pyspark.ml.evaluation import RegressionEvaluator
+
+regEval = RegressionEvaluator().setLabelCol('label')
+crossval = CrossValidator(estimator=model,
+                          evaluator=regEval,
+                          numFolds=3)
+
+# Re-fitting on the entire training set
+cvModel = crossval.fit(trainingDF)
+
+#################################################################################################################
+##### Hyperparameter Tuning
+# Grid Search
 from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
 
 # Defining our parameter grid
