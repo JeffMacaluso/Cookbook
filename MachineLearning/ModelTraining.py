5974b3618a5abefc2680e4b9e4855bb3bd1f4b52
Jeff Macaluso <JeffMacaluso@users.noreply.github.com>
2018-08-09 15:47:43 -0500

    Added additional import statement

--- MachineLearning/ModelTraining.py
+++ MachineLearning/ModelTraining.py
@@ -32,7 +32,7 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=46)
 
 # K-fold cross validation
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, cross_val_score
 k_fold = KFold(n_splits=10, shuffle=True, random_state=46)
 cross_val_score(model, X, y, cv=k_fold, n_jobs=-1)
 
