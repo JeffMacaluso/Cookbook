02e71edbcdf1c9ce6b12203298eddad8565178f1
Jeff Macaluso <JeffMacaluso@users.noreply.github.com>
2019-08-16 14:24:53 -0700

    Added confusion matrix

--- MachineLearning/EvaluationPlots.py
+++ MachineLearning/EvaluationPlots.py
@@ -213,3 +213,50 @@ def explain_features_shap(model, features, max_features_to_show=15):
         print('SHAP values for individual features')
         for name in features.columns:
             shap.dependence_plot(name, shap_values, features, display_features=features)
+
+            
+# Confusion matrix
+def plot_confusion_matrix(label, predictions, classes, normalize=False):
+    '''
+    TODO: Write docstring
+    '''
+    import itertools
+    from sklearn import metrics
+    
+    # Calculating the confusion matrix
+    cm = metrics.confusion_matrix(label, predictions)
+    
+    # Normalizing if specified
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print('Normalized confusion matrix')
+    else:
+        print('Confusion matrix')
+
+    print(cm)
+
+    # Plotting the confusion matrix
+    plt.figure(figsize=(7, 7))
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title('Confusion Matrix')
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    # Adding the text properties to the graph
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.0
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(
+            j,
+            i,
+            format(cm[i, j], fmt),
+            horizontalalignment='center',
+            color='white' if cm[i, j] > thresh else 'black',
+        )
+
+    # Additional graph properties
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
